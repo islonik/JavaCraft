1136d9e0cbebe846f805c4684223511177753202
nikita.lipatov <nikita.lipatov@gmail.com>
2023-08-01 19:24:02 +0100

    fix H2 console access

--- soap2rest/rest/rest-app/src/main/java/my/javacraft/soap2rest/rest/app/conf/SecurityConfiguration.java
+++ soap2rest/rest/rest-app/src/main/java/my/javacraft/soap2rest/rest/app/conf/SecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -18,6 +19,10 @@ public class SecurityConfiguration {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        // disable 'X-Frame-Options' for H2 /console access
+        http.headers((headersConfigurer) -> headersConfigurer.frameOptions(FrameOptionsConfig::disable));
+
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorizeHttpRequests) ->
                         authorizeHttpRequests
