fa6c59b52a2a93a149215f6a01182c146787409e
nikita.lipatov <nikita.lipatov@gmail.com>
2023-09-13 22:52:53 +0100

    refactoring

--- mathparser/parser/src/main/java/my/javacraft/mathparser/parser/RomanNumbers.java
+++ mathparser/parser/src/main/java/my/javacraft/mathparser/parser/RomanNumbers.java
@@ -13,49 +13,49 @@ package my.javacraft.mathparser.parser;
  */
 public class RomanNumbers {
 
-    final static String[] Thousands = {"", "M", "MM", "MMM", "MMMM"};
-    final static String[] Hundreds = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
-    final static String[] Tens = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
-    final static String[] Units = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
+    private final static String[] THOUSANDS = {"", "M", "MM", "MMM", "MMMM"};
+    private final static String[] HUNDREDS = {"", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"};
+    private final static String[] TENS = {"", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"};
+    private final static String[] UNITS = {"", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"};
 
     static String toRoman(int number) {
         StringBuilder roman = new StringBuilder();
-        roman.append(Thousands[number / 1000]);
-        roman.append(Hundreds[(number % 1000) / 100]);
-        roman.append(Tens[(number % 100) / 10]);
-        roman.append(Units[number % 10]);
+        roman.append(THOUSANDS[number / 1000]);
+        roman.append(HUNDREDS[(number % 1000) / 100]);
+        roman.append(TENS[(number % 100) / 10]);
+        roman.append(UNITS[number % 10]);
         return roman.toString();
     }
 
     static int fromRoman(String romanNumber) {
         int number = 0;
 
-        for (int i = Thousands.length - 1; i >= 0; i--) {
-            if (romanNumber.startsWith(Thousands[i])) {
+        for (int i = THOUSANDS.length - 1; i >= 0; i--) {
+            if (romanNumber.startsWith(THOUSANDS[i])) {
                 number += i * 1000;
-                romanNumber = romanNumber.substring(Thousands[i].length());
+                romanNumber = romanNumber.substring(THOUSANDS[i].length());
                 break;
             }
         }
 
-        for (int i = Hundreds.length - 1; i >= 0; i--) {
-            if (romanNumber.startsWith(Hundreds[i])) {
+        for (int i = HUNDREDS.length - 1; i >= 0; i--) {
+            if (romanNumber.startsWith(HUNDREDS[i])) {
                 number += i * 100;
-                romanNumber = romanNumber.substring(Hundreds[i].length());
+                romanNumber = romanNumber.substring(HUNDREDS[i].length());
                 break;
             }
         }
 
-        for (int i = Tens.length - 1; i >= 0; i--) {
-            if (romanNumber.startsWith(Tens[i])) {
+        for (int i = TENS.length - 1; i >= 0; i--) {
+            if (romanNumber.startsWith(TENS[i])) {
                 number += i * 10;
-                romanNumber = romanNumber.substring(Tens[i].length());
+                romanNumber = romanNumber.substring(TENS[i].length());
                 break;
             }
         }
 
-        for (int i = Units.length - 1; i >= 0; i--) {
-            if (romanNumber.startsWith(Units[i])) {
+        for (int i = UNITS.length - 1; i >= 0; i--) {
+            if (romanNumber.startsWith(UNITS[i])) {
                 number += i;
                 break;
             }
