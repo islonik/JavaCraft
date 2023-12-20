ef214f11551249d395857f51b51e3f73f2dbd569
nikita.lipatov <nikita.lipatov@gmail.com>
2023-12-20 19:12:45 +0000

    refactoring

--- soap2rest/rest/rest-app/src/main/java/my/javacraft/soap2rest/rest/app/Application.java
+++ soap2rest/rest/rest-app/src/main/java/my/javacraft/soap2rest/rest/app/Application.java
@@ -19,8 +19,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 })
 public class Application {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        log.info("REST Application starting...");
         SpringApplication.run(Application.class, args);
-        log.info("REST Application started.");
     }
 }
