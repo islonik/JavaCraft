0e1730a20d8a1a806a34539d642463b8e189c8e4
nikita.lipatov <nikita.lipatov@gmail.com>
2024-04-15 11:15:11 +0100

    add cron expression comments

--- elastic/src/main/java/my/javacraft/elastic/scheduler/SchedulerJobs.java
+++ elastic/src/main/java/my/javacraft/elastic/scheduler/SchedulerJobs.java
@@ -8,6 +8,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+/**
+ * Cron expression: second, minute, hour, day of month, month, day(s) of week
+ * ┏━━━━━━━━━━━━ Second (0-59)
+ * ┃ ┏━━━━━━━━━━ Minute (0-59)
+ * ┃ ┃ ┏━━━━━━━━ Hour (0-23)
+ * ┃ ┃ ┃ ┏━━━━━━ Day of month (1-31)
+ * ┃ ┃ ┃ ┃ ┏━━━━ Month (1-12)
+ * ┃ ┃ ┃ ┃ ┃ ┏━━ Day(s) of week (1-7, Monday to Sunday)
+ * * * * * * *
+ * <p/>
+ * Examples:
+ * 0 0    *    *  *  *      - the top of every hour of every day
+ * 0 0    8-10 *  *  *      - 8,9,10 o'clock of every day
+ * 0 0    8,10 *  *  *      - 8 and 10 o'clock of every day
+ * 0 0/30 8-10 *  *  *      - 8:00, 8:30, 9:00, and 10 o'clock every day
+ * 0 0    9-17 *  * MON-FRI - on the hour nine-to-five weekdays
+ * 0 0    0    25 12 ?      - every Christmas Day at midnight
+ */
 @Slf4j
 @Configuration
 @EnableScheduling
