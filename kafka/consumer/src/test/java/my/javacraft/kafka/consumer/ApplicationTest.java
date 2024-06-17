9621ab5c1ac644866404a05983fbd975e1ce06db
nikita.lipatov <nikita.lipatov@gmail.com>
2024-06-17 15:06:48 +0100

    update Kafka README.md

--- kafka/consumer/src/test/java/my/javacraft/kafka/consumer/ApplicationTest.java
+++ kafka/consumer/src/test/java/my/javacraft/kafka/consumer/ApplicationTest.java
@@ -1,38 +1,8 @@
 package my.javacraft.kafka.consumer;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 /**
  * Unit test for simple App.
  */
-public class ApplicationTest
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public ApplicationTest(String testName )
-    {
-        super( testName );
-    }
+public class ApplicationTest {
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( ApplicationTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
