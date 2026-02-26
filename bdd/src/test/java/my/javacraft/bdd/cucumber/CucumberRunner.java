782d8d5c25773e3ae28beb51b325ceb2f61f698d
nikita.lipatov <nikita.lipatov@gmail.com>
2026-02-26 20:59:23 +0000

    bdd fixes - remove warnings & execute junit5 tests

--- bdd/src/test/java/my/javacraft/bdd/cucumber/CucumberRunner.java
+++ bdd/src/test/java/my/javacraft/bdd/cucumber/CucumberRunner.java
@@ -2,15 +2,15 @@ package my.javacraft.bdd.cucumber;
 
 import org.junit.platform.suite.api.ConfigurationParameter;
 import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
 import org.junit.platform.suite.api.Suite;
 
-import static io.cucumber.junit.platform.engine.Constants.FEATURES_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
 @Suite
 @IncludeEngines("cucumber")
-@ConfigurationParameter(key = FEATURES_PROPERTY_NAME, value = "classpath:features")
+@SelectPackages("features")
 @ConfigurationParameter(key = PLUGIN_PROPERTY_NAME,
         value = "pretty, html:target/cucumber-reports/cucumber.html, json:target/cucumber-reports/cucumber.json")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "my.javacraft.bdd.cucumber")
