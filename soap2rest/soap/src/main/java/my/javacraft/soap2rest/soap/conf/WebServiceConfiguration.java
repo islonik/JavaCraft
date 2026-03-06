f04bc4a9290f056e3acb4973611d7c428e7ab5eb
nikita.lipatov <nikita.lipatov@gmail.com>
2026-03-06 13:08:06 +0000

    soap2rest fixes - migrate deprecated WsConfigurerAdapter to WsConfigurer

--- soap2rest/soap/src/main/java/my/javacraft/soap2rest/soap/conf/WebServiceConfiguration.java
+++ soap2rest/soap/src/main/java/my/javacraft/soap2rest/soap/conf/WebServiceConfiguration.java
@@ -6,14 +6,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.ws.config.annotation.EnableWs;
-import org.springframework.ws.config.annotation.WsConfigurerAdapter;
+import org.springframework.ws.config.annotation.WsConfigurer;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.Wsdl11Definition;
 
 @EnableWs
 @Configuration
-public class WebServiceConfiguration extends WsConfigurerAdapter {
+public class WebServiceConfiguration implements WsConfigurer {
 
     @Bean
     public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(
@@ -24,10 +24,7 @@ public class WebServiceConfiguration extends WsConfigurerAdapter {
         return new ServletRegistrationBean<>(servlet, "/soap2rest/soap/v1/*");
     }
 
-    /**
-     *
-     * http://localhost:8078/soap2rest/soap/v1/DeliverServiceWS.wsdl
-     */
+    // http://localhost:8078/soap2rest/soap/v1/DeliverServiceWS.wsdl
     @Bean(name = "DeliverServiceWS")
     public Wsdl11Definition defaultWsdl11Definition() {
         SimpleWsdl11Definition wsdl11Definition = new SimpleWsdl11Definition();
