d01fa3356e2760d324bd0a76d14892b31ae9407e
nikita.lipatov <nikita.lipatov@gmail.com>
2026-03-07 06:23:27 +0000

    soap2rest fixes - refactor in soap module cucumber tests in ElectricService.feature

--- soap2rest/soap/src/test/java/my/javacraft/soap2rest/soap/cucumber/step/WireMockDefinition.java
+++ soap2rest/soap/src/test/java/my/javacraft/soap2rest/soap/cucumber/step/WireMockDefinition.java
@@ -2,13 +2,16 @@ package my.javacraft.soap2rest.soap.cucumber.step;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
+import java.util.List;
+import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
+import static com.github.tomakehurst.wiremock.stubbing.Scenario.STARTED;
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
 
@@ -16,25 +19,44 @@ import static io.cucumber.spring.CucumberTestContext.SCOPE_CUCUMBER_GLUE;
 @Scope(SCOPE_CUCUMBER_GLUE)
 public class WireMockDefinition {
 
+    private static final String FIRST_METRIC_ADDED = "FIRST_METRIC_ADDED";
+    private static final String SECOND_METRIC_ADDED = "SECOND_METRIC_ADDED";
+    private static final String THIRD_METRIC_ADDED = "THIRD_METRIC_ADDED";
+    private static final String ELECTRIC_METRIC_TEMPLATE = "electric/metric.json.hbs";
+    private static final String ELECTRIC_METRICS_TEMPLATE = "electric/metrics.json.hbs";
+
     @Value("${rest-app.port}")
     int restPort;
 
-    WireMockServer wireMockServer;
+    private static final Object LOCK = new Object();
+    private static WireMockServer wireMockServer;
 
     @Given("we start WireMock server")
     public void setup () {
-        try {
-            wireMockServer = new WireMockServer(restPort);
+        synchronized (LOCK) {
+            try {
+                if (wireMockServer == null) {
+                    wireMockServer = new WireMockServer(
+                            options().port(restPort).templatingEnabled(true).globalTemplating(true)
+                    );
+                }
 
-            wireMockServer.start();
+                if (!wireMockServer.isRunning()) {
+                    wireMockServer.start();
+                } else {
+                    wireMockServer.resetAll();
+                }
 
-            addGasStubs(wireMockServer);
-            addElectricStubs(wireMockServer);
-            addSmartStubs(wireMockServer);
+                wireMockServer.resetScenarios();
 
-            log.info("Setup Stubs is done for MetricService");
-        } catch (Exception e) {
-            log.info("WireMock start exception: " + e.getMessage());
+                addGasStubs(wireMockServer);
+                addElectricStubs(wireMockServer);
+                addSmartStubs(wireMockServer);
+
+                log.info("Setup Stubs is done for MetricService");
+            } catch (Exception e) {
+                log.error("WireMock setup failed", e);
+            }
         }
     }
 
@@ -66,29 +88,149 @@ public class WireMockDefinition {
     }
 
     void addElectricStubs(WireMockServer wireMockServer) {
-        wireMockServer.stubFor(put(urlEqualTo("/api/v1/smart/1/electric"))
-                .willReturn(aResponse().withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withStatus(200)
-                        .withBodyFile("put_1_electric.json")
+        addElectricStubsForAccount(
+                wireMockServer,
+                "1",
+                List.of(
+                        electricMetric(13, 100, "678.439", "2023-07-28"),
+                        electricMetric(14, 100, "700.111", "2023-07-29"),
+                        electricMetric(15, 100, "720.333", "2023-07-30")
                 )
         );
-        wireMockServer.stubFor(delete(urlEqualTo("/api/v1/smart/1/electric"))
-                .willReturn(aResponse().withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withStatus(200)
-                        .withBody("true")
+
+        addElectricStubsForAccount(
+                wireMockServer,
+                "2",
+                List.of(
+                        electricMetric(21, 220, "54.321", "2024-01-15"),
+                        electricMetric(22, 220, "60.999", "2024-01-16"),
+                        electricMetric(23, 220, "61.222", "2024-01-17")
                 )
         );
-        wireMockServer.stubFor(get(urlEqualTo("/api/v1/smart/1/electric/latest"))
-                .willReturn(aResponse().withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                        .withStatus(200)
-                        .withBodyFile("put_1_electric.json")
-                )
+    }
+
+    void addElectricStubsForAccount(
+            WireMockServer wireMockServer,
+            String accountId,
+            List<Map<String, Object>> metrics
+    ) {
+        String scenarioName = "electric-flow-account-" + accountId;
+        String baseUrl = "/api/v1/smart/" + accountId + "/electric";
+
+        addDeleteStubForState(wireMockServer, scenarioName, baseUrl, STARTED);
+        addDeleteStubForState(wireMockServer, scenarioName, baseUrl, FIRST_METRIC_ADDED);
+        addDeleteStubForState(wireMockServer, scenarioName, baseUrl, SECOND_METRIC_ADDED);
+        addDeleteStubForState(wireMockServer, scenarioName, baseUrl, THIRD_METRIC_ADDED);
+
+        wireMockServer.stubFor(put(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .willSetStateTo(FIRST_METRIC_ADDED)
+                .willReturn(withTemplateMetric(metrics.getFirst()))
         );
-        wireMockServer.stubFor(get(urlEqualTo("/api/v1/smart/1/electric"))
+
+        wireMockServer.stubFor(put(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(FIRST_METRIC_ADDED)
+                .willSetStateTo(SECOND_METRIC_ADDED)
+                .willReturn(withTemplateMetric(metrics.get(1)))
+        );
+
+        wireMockServer.stubFor(put(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(SECOND_METRIC_ADDED)
+                .willSetStateTo(THIRD_METRIC_ADDED)
+                .willReturn(withTemplateMetric(metrics.get(2)))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl + "/latest"))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .willReturn(aResponse().withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                        .withStatus(200))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl + "/latest"))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(FIRST_METRIC_ADDED)
+                .willReturn(withTemplateMetric(metrics.getFirst()))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl + "/latest"))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(SECOND_METRIC_ADDED)
+                .willReturn(withTemplateMetric(metrics.get(1)))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl + "/latest"))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(THIRD_METRIC_ADDED)
+                .willReturn(withTemplateMetric(metrics.get(2)))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(STARTED)
+                .willReturn(withTemplateMetrics(List.of()))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(FIRST_METRIC_ADDED)
+                .willReturn(withTemplateMetrics(metrics.subList(0, 1)))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(SECOND_METRIC_ADDED)
+                .willReturn(withTemplateMetrics(metrics.subList(0, 2)))
+        );
+
+        wireMockServer.stubFor(get(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(THIRD_METRIC_ADDED)
+                .willReturn(withTemplateMetrics(metrics))
+        );
+    }
+
+    Map<String, Object> electricMetric(int id, int meterId, String reading, String date) {
+        return Map.of(
+                "id", id,
+                "meterId", meterId,
+                "reading", reading,
+                "date", date
+        );
+    }
+
+    com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder withTemplateMetric(
+            Map<String, Object> metric) {
+        return aResponse()
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .withStatus(200)
+                .withBodyFile(ELECTRIC_METRIC_TEMPLATE)
+                .withTransformers("response-template")
+                .withTransformerParameter("metric", metric);
+    }
+
+    com.github.tomakehurst.wiremock.client.ResponseDefinitionBuilder withTemplateMetrics(
+            List<Map<String, Object>> metrics) {
+        return aResponse()
+                .withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .withStatus(200)
+                .withBodyFile(ELECTRIC_METRICS_TEMPLATE)
+                .withTransformers("response-template")
+                .withTransformerParameter("metrics", metrics);
+    }
+
+    void addDeleteStubForState(
+            WireMockServer wireMockServer, String scenarioName, String baseUrl, String state) {
+        wireMockServer.stubFor(delete(urlEqualTo(baseUrl))
+                .inScenario(scenarioName)
+                .whenScenarioStateIs(state)
+                .willSetStateTo(STARTED)
                 .willReturn(aResponse().withHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
                         .withStatus(200)
-                        .withBodyFile("get_1_electric.json")
-                )
+                        .withBody("true"))
         );
     }
 
@@ -118,7 +260,5 @@ public class WireMockDefinition {
                 )
         );
     }
-
-
 
 }
--- soap2rest/soap/src/test/resources/features/ElectricService.feature
+++ soap2rest/soap/src/test/resources/features/ElectricService.feature
@@ -1,7 +1,43 @@
 Feature: ElectricService
-  Scenario: submit & check latest electric metric
-    Given we start WireMock server
-    When we send a SOAP request to delete all previous electric metrics
-    When we send a SOAP request to put a new electric metric
-    Then we send a SOAP request to get the latest electric metric
-    Then we send a SOAP request to get all electric metrics
+
+  Rule: Sync electric service behavior
+
+    @Sync
+    Scenario: test a single user with incremental metric updates
+      Given we start WireMock server
+      When user "1" deletes all previous electric metrics
+      Then user "1" has no latest electric metric
+      And user "1" has electric metrics list size "0"
+      When user "1" puts electric metric id "13" meter "100" reading "678.439" date "2023-07-28"
+      Then user "1" gets latest electric metric id "13" meter "100" reading "678.439" date "2023-07-28"
+      And user "1" has electric metrics list size "1"
+      When user "1" puts electric metric id "14" meter "100" reading "700.111" date "2023-07-29"
+      Then user "1" gets latest electric metric id "14" meter "100" reading "700.111" date "2023-07-29"
+      And user "1" has electric metrics list size "2"
+
+    @Sync
+    Scenario: test user isolation with three metrics per user
+      Given we start WireMock server
+      When user "1" deletes all previous electric metrics
+      Then user "1" has no latest electric metric
+      And user "1" has electric metrics list size "0"
+      When user "1" puts electric metrics
+        | id | meterId | reading | date       |
+        | 13 | 100     | 678.439 | 2023-07-28 |
+        | 14 | 100     | 700.111 | 2023-07-29 |
+        | 15 | 100     | 720.333 | 2023-07-30 |
+      Then user "1" gets latest electric metric id "15" meter "100" reading "720.333" date "2023-07-30"
+      And user "1" has electric metrics list size "3"
+
+      When user "2" deletes all previous electric metrics
+      Then user "2" has no latest electric metric
+      And user "2" has electric metrics list size "0"
+      When user "2" puts electric metrics
+        | id | meterId | reading | date       |
+        | 21 | 220     | 54.321  | 2024-01-15 |
+        | 22 | 220     | 60.999  | 2024-01-16 |
+        | 23 | 220     | 61.222  | 2024-01-17 |
+      Then user "2" gets latest electric metric id "23" meter "220" reading "61.222" date "2024-01-17"
+      And user "2" has electric metrics list size "3"
+      And user "1" gets latest electric metric id "15" meter "100" reading "720.333" date "2023-07-30"
+      And user "1" has electric metrics list size "3"
