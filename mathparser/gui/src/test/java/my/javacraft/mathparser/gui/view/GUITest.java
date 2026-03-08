01daf789f292f0da2f5d08f7507fa0fed05ede9d
nikita.lipatov <nikita.lipatov@gmail.com>
2026-03-08 12:18:43 +0000

    mathparser fixes - remove mockito in GUITest

--- mathparser/gui/src/test/java/my/javacraft/mathparser/gui/view/GUITest.java
+++ mathparser/gui/src/test/java/my/javacraft/mathparser/gui/view/GUITest.java
@@ -1,10 +1,11 @@
 package my.javacraft.mathparser.gui.view;
 
+import java.awt.GraphicsEnvironment;
 import java.awt.event.*;
 import my.javacraft.mathparser.parser.Parser;
+import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 
 /**
  * User: Lipatov Nikita
@@ -13,28 +14,29 @@ public class GUITest {
 
     @Test
     public void testCase() {
+        Assumptions.assumeFalse(GraphicsEnvironment.isHeadless(), "Requires graphics environment");
         Parser mathParser = new Parser();
         GUI instance = new GUI();
-        instance.setMathParser(mathParser);
+        try {
+            instance.setMathParser(mathParser);
 
-        ActionEvent event1 = Mockito.mock(ActionEvent.class);
-        Mockito.when(event1.getActionCommand()).thenReturn("1");
-        instance.actionPerformed(event1);
+            ActionEvent event1 = new ActionEvent(instance, ActionEvent.ACTION_PERFORMED, "1");
+            instance.actionPerformed(event1);
 
-        ActionEvent event2 = Mockito.mock(ActionEvent.class);
-        Mockito.when(event2.getActionCommand()).thenReturn("0");
-        instance.actionPerformed(event2);
+            ActionEvent event2 = new ActionEvent(instance, ActionEvent.ACTION_PERFORMED, "0");
+            instance.actionPerformed(event2);
 
-        ActionEvent event3 = Mockito.mock(ActionEvent.class);
-        Mockito.when(event3.getActionCommand()).thenReturn("+");
-        instance.actionPerformed(event3);
+            ActionEvent event3 = new ActionEvent(instance, ActionEvent.ACTION_PERFORMED, "+");
+            instance.actionPerformed(event3);
 
-        ActionEvent event4 = Mockito.mock(ActionEvent.class);
-        Mockito.when(event4.getActionCommand()).thenReturn("9");
-        instance.actionPerformed(event4);
+            ActionEvent event4 = new ActionEvent(instance, ActionEvent.ACTION_PERFORMED, "9");
+            instance.actionPerformed(event4);
 
-        instance.calculateButton();
+            instance.calculateButton();
 
-        Assertions.assertEquals("19.0", instance.getOutputText());
+            Assertions.assertEquals("19.0", instance.getOutputText());
+        } finally {
+            instance.dispose();
+        }
     }
 }
